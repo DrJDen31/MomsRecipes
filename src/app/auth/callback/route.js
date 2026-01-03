16081d50fa326850b4b5b5f8d21d73918a4a8e42
Jaden Tompkins <jadentompkins31@gmail.com>
2026-01-02 22:36:42 -0500

    auth on mobile

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -6,6 +6,8 @@ import { NextResponse } from 'next/server'
 export async function GET(request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
 
@@ -47,6 +49,53 @@ export async function GET(request) {
       }
     } else {
       console.error('Auth code exchange error:', error)
+      return NextResponse.redirect(`${origin}/auth/auth-code-error?error=${encodeURIComponent(error.message)}`)
+    }
+  }
+
+  if (token_hash && type) {
+    const cookieStore = await cookies()
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll()
+          },
+          setAll(cookiesToSet) {
+            try {
+              cookiesToSet.forEach(({ name, value, options }) =>
+                cookieStore.set(name, value, options)
+              )
+            } catch {
+              // The `setAll` method was called from a Server Component.
+              // This can be ignored if you have middleware refreshing
+              // user sessions.
+            }
+          },
+        },
+      }
+    )
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+
+    if (!error) {
+       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+       const isLocalEnv = process.env.NODE_ENV === 'development'
+       if (isLocalEnv) {
+         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+         return NextResponse.redirect(`${origin}${next}`)
+       } else if (forwardedHost) {
+         return NextResponse.redirect(`https://${forwardedHost}${next}`)
+       } else {
+         return NextResponse.redirect(`${origin}${next}`)
+       }
+    } else {
+      console.error('Auth token exchange error:', error)
       return NextResponse.redirect(`${origin}/auth/auth-code-error?error=${encodeURIComponent(error.message)}`)
     }
   }
